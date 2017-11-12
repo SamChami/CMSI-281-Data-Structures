4942e16950b183cf0b02779c2ffa515df8d04cc8
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-11-12 00:19:12 -0800

    Changed function ordering

--- WebNavigator/WebNavigator.java
+++ WebNavigator/WebNavigator.java
@@ -12,8 +12,8 @@ public class WebNavigator {
 
     // Constructor
     WebNavigator () {
-    	backwardHistory = new LinkedList<String>();
-    	forwardHistory = new LinkedList<String>();
+        backwardHistory = new LinkedList<String>();
+        forwardHistory = new LinkedList<String>();
     }
 
     // Methods
@@ -23,20 +23,20 @@ public class WebNavigator {
 
         // Switch on the command (issued first in input line)
         switch(parsedCommand[0]) {
-        case "exit":
-            System.out.println("Goodbye!");
-            return false;
-        case "visit":
-            visit(parsedCommand[1]);
-            break;
-        case "back":
-            back();
-            break;
-        case "forward":
-            forw();
-            break;
-        default:
-            System.out.println("[X] Invalid command, try again");
+            case "exit":
+                System.out.println("Goodbye!");
+                return false;
+            case "visit":
+                visit(parsedCommand[1]);
+                break;
+            case "back":
+                back();
+                break;
+            case "forward":
+                forw();
+                break;
+            default:
+                System.out.println("[X] Invalid command, try again");
         }
 
         System.out.println("Currently Visiting: " + current);
@@ -45,9 +45,9 @@ public class WebNavigator {
     }
 
     /*
-     *  Visits the current site, clears the forward history,
-     *  and records the visited site in the back history
-     */
+    *  Visits the current site, clears the forward history,
+    *  and records the visited site in the back history
+    */
     public void visit (String site) {
         backwardHistory.add(current);
         current = site;
@@ -55,23 +55,23 @@ public class WebNavigator {
     }
 
     /*
-     *  Changes the current site to the one that was last
-     *  visited in the order on which visit was called on it
-     */
+    *  Changes the current site to the one that was last
+    *  visited in the order on which visit was called on it
+    */
     public void back () {
-    	if(backwardHistory.size() > 1) {
-	    	forwardHistory.addFirst(current);
-	      current = backwardHistory.getLast();
-	      backwardHistory.removeLast();
-    	}
+        if(backwardHistory.size() > 1) {
+            forwardHistory.addFirst(current);
+            current = backwardHistory.getLast();
+            backwardHistory.removeLast();
+        }
     }
 
     public void forw () {
-    	if (forwardHistory.size() > 0) {
-        backwardHistory.add(current);
-        current = forwardHistory.getFirst();
-        forwardHistory.removeFirst();
-    	}
+        if (forwardHistory.size() > 0) {
+            backwardHistory.add(current);
+            current = forwardHistory.getFirst();
+            forwardHistory.removeFirst();
+        }
     }
 
     public static void main(String[] args) {
@@ -81,6 +81,6 @@ public class WebNavigator {
         System.out.println("Welcome to ForneyFox, enter a command from your ForneyFox user manual!");
         while (navi.getNextUserCommand(input)) {}
         System.out.println("Goodbye!");
-    }
+        }
 
-}
+    }
